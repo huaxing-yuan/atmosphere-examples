c7333a6a2eeebae085bc6df7644bfb2b2ae1ccec
Huaxing YUAN <x_sliver@163.com>
2023-06-19 00:22:43 +0200

    add a soap service example

---
+++ SoapServiceExample/ts_Simple Soap Services/Simple Soap Services.ts
@@ -0,0 +1,24 @@
+﻿{
+  "FileName": "ts_Simple Soap Services\\Simple Soap Services.ts",
+  "TestCasesOrder": [
+    {
+      "Key": "188e5ef0-e806-4c56-be25-8937a9282963",
+      "Value": "ts_Simple Soap Services\\Call Soap Service.tc"
+    }
+  ],
+  "ErrorBehavior": "RunNextTestCase",
+  "AvailableErrorBehaviors": {
+    "$type": "Atmosphere.Automation.TestSuiteErrorBehavior[], Atmosphere.Automation",
+    "$values": [
+      "RunNextTestCase",
+      "StopTestSuite"
+    ]
+  },
+  "IsExpanded": true,
+  "Information": "",
+  "Id": "c13ed80f-4261-4f8c-b0b0-4e073e3232d3",
+  "Name": "Simple Soap Services",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
