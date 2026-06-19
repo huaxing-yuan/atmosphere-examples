2faae140378c3e6fabd6758ddd6ad83849ccfae1
Huaxing YUAN <x_sliver@163.com>
2026-06-20 00:46:10 +0200

    add new service

---
+++ Example Project/ts_Flight Registration/Flight Registration.ts
@@ -0,0 +1,15 @@
+﻿{
+  "TestCasesOrder": [
+    {
+      "Name": "226ffca4-93b0-4f11-8702-4b75f5d24f04",
+      "Value": "ts_Flight Registration/Register Flight.tc"
+    }
+  ],
+  "ErrorBehavior": "RunNextTestCase",
+  "IsExpanded": true,
+  "Id": "5811b3d2-7596-48b0-bbc7-d7c58db3a778",
+  "Name": "Flight Registration",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
