8786d8199c5962f19e1eaf8833cb8bdad064e623
Huaxing YUAN <x_sliver@163.com>
2026-06-23 20:22:04 +0200

    + ts3

---
+++ Example Project/ts_Test Suite 3/Test Suite 3.ts
@@ -0,0 +1,10 @@
+﻿{
+  "TestCasesOrder": [],
+  "ErrorBehavior": "RunNextTestCase",
+  "IsExpanded": false,
+  "Id": "c558e3e6-84b2-495d-ae8f-8ca35884ce82",
+  "Name": "Test Suite 3",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
