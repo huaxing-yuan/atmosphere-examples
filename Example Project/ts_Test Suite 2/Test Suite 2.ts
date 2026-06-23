77205a1f37f2acaf3ac34dc410a46ffbccca082b
Huaxing YUAN <x_sliver@163.com>
2026-06-23 19:29:20 +0200

    add ts

---
+++ Example Project/ts_Test Suite 2/Test Suite 2.ts
@@ -0,0 +1,10 @@
+﻿{
+  "TestCasesOrder": [],
+  "ErrorBehavior": "RunNextTestCase",
+  "IsExpanded": false,
+  "Id": "743da811-7607-4f22-9c30-19c8b3638ad3",
+  "Name": "Test Suite 2",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
