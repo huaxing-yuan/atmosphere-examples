031e724fc2db777fb691be7b00a319c525385059
Huaxing YUAN <x_sliver@163.com>
2026-06-23 20:25:32 +0200

    ts4

---
+++ Example Project/ts_Test Suite 4/Test Suite 4.ts
@@ -0,0 +1,10 @@
+﻿{
+  "TestCasesOrder": [],
+  "ErrorBehavior": "RunNextTestCase",
+  "IsExpanded": false,
+  "Id": "d0dee5a5-1d9e-4323-8ba2-fb830652eae2",
+  "Name": "Test Suite 4",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
