a8ee043b92ba3cf3ed8f61f8b87cd43cc7cc749e
Huaxing YUAN <x_sliver@163.com>
2026-06-23 18:54:58 +0200

    add new test

---
+++ Example Project/ts_Test Suite 1/Test Suite 1.ts
@@ -0,0 +1,15 @@
+﻿{
+  "TestCasesOrder": [
+    {
+      "Name": "339bc4a4-e9f8-43f9-844f-e0800a1dd257",
+      "Value": "ts_Test Suite 1/My Case 1.tc"
+    }
+  ],
+  "ErrorBehavior": "RunNextTestCase",
+  "IsExpanded": true,
+  "Id": "9daee765-9e7e-4d5a-b486-5538323c91cf",
+  "Name": "Test Suite 1",
+  "Description": "",
+  "IsEnabled": true,
+  "LocalVariables": []
+}
